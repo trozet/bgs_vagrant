c9eac14afa055a96c39758db0df93f1d03a9c315
Tim Rozet <tdrozet@gmail.com>
2015-04-29 16:23:11 -0400

    Fixes issue #5 - make bootstrap.sh more robust
    Changes include:
     - Remove sudo
     - Remove ksgen setup
     - Add failure detection to commands

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,42 +1,80 @@
 #!/usr/bin/env bash
 
-#sudo yum -y update
+#bootstrap script for installing/running Khaleesi in Foreman/QuickStack VM
+#author: Tim Rozet (trozet@redhat.com)
+#
+#Uses Vagrant and VirtualBox
+#VagrantFile uses bootsrap.sh which Installs Khaleesi
+#Khaleesi will install and configure Foreman/QuickStack
+#
+#Pre-requisties:
+#Target system should be Centos7
+#Ensure the host's kernel is up to date (yum update)
 
-sudo rpm -i http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+##VARS
+reset=`tput sgr0`
+blue=`tput setaf 4`
+red=`tput setaf 1`
+green=`tput setaf 2`
 
-sudo yum -y install python-pip python-virtualenv gcc git
+##END VARS
 
-sudo yum -y install python-keystoneclient python-novaclient python-glanceclient python-neutronclient python-keystoneclient
 
-yum -y install sshpass
+##install EPEL
+if ! yum repolist | grep "epel/"; then
+  if ! rpm -Uvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm; then
+    printf '%s\n' 'bootstrap.sh: Unable to configure EPEL repo' >&2
+    exit 1
+  fi
+else
+  printf '%s\n' 'bootstrap.sh: Skipping EPEL repo as it is already configured.'
+fi
+
+##install python,gcc,git
+if ! yum -y install python-pip python-virtualenv gcc git; then
+  printf '%s\n' 'bootstrap.sh: Unable to install python,gcc,git packages' >&2
+  exit 1
+fi
+
+##Install openstack clients
+if ! yum -y install python-keystoneclient python-novaclient python-glanceclient python-neutronclient; then
+  printf '%s\n' 'bootstrap.sh: Unable to install python openstack client packages' >&2
+  exit 1
+fi
+
+##Install sshpass
+if ! yum -y install sshpass; then
+  printf '%s\n' 'bootstrap.sh: Unable to install sshpass' >&2
+  exit 1
+fi
 
 cd /opt
 
 echo "Cloning khaleesi to /opt"
 
 if [ ! -d khaleesi ]; then
-  sudo git clone -b opnfv https://github.com/trozet/khaleesi.git
+  if ! git clone -b opnfv https://github.com/trozet/khaleesi.git; then
+    printf '%s\n' 'bootstrap.sh: Unable to git clone khaleesi' >&2
+    exit 1
+  fi
 fi
 
+if ! pip install ansible; then
+  printf '%s\n' 'bootstrap.sh: Unable to install ansible' >&2
+  exit 1
+fi
 
-pip install ansible
-pip install requests
-
+if ! pip install requests; then
+  printf '%s\n' 'bootstrap.sh: Unable to install requests python package' >&2
+  exit 1
+fi
 
 
 cd khaleesi
 
-sudo cp ansible.cfg.example ansible.cfg
-
-cd tools/ksgen
-
-sudo python setup.py develop
-
-cd ../..
+cp ansible.cfg.example ansible.cfg
 
 echo "Completed Installing Khaleesi"
-
-cd /opt
 
 cd /opt/khaleesi/
 
