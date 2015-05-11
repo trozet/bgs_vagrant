7ea797c73cec64d4a93d030ef757cddbca24c151
Tim Rozet <tdrozet@gmail.com>
2015-05-11 15:34:39 -0400

    Preparing for merge to master

--- deploy.sh
+++ deploy.sh
@@ -527,7 +527,7 @@ for node in ${nodes}; do
 
   ##clone bgs vagrant
   ##will change this to be opnfv repo when commit is done
-  if ! git clone https://github.com/trozet/bgs_vagrant.git -b virtual $node; then
+  if ! git clone https://github.com/trozet/bgs_vagrant.git $node; then
     printf '%s\n' 'deploy.sh: Unable to clone vagrant repo' >&2
     exit 1
   fi
